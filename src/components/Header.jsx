722afdfffd013c04d0956c21fcf25c6820d6c2ed
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2020-06-09 17:51:51 -0300

    change header for head

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => (
-  <header>
+  <head>
     <meta charSet='UTF-8' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     {/* displays site properly based on user's device */}
@@ -9,7 +9,7 @@ const Header = () => (
     <title> Frontend Mentor | Huddle landing page with single introductory section </title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css' />
 
-  </header>
+  </head>
 );
 
 export default Header;
