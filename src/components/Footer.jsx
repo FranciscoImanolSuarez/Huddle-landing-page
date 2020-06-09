1ca8f13f4aa16511574af751dd02145334d8a548
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2020-06-09 17:48:21 -0300

    Add links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 const Footer = () => (
   <ul className='social'>
     <li>
-      <a href='#'>
+      <a href='https://www.facebook.com/frontendmentor'>
         <i className='fab fa-facebook-f' />
       </a>
     </li>
     <li>
-      <a href='#'>
+      <a href='https://twitter.com/frontendmentor'>
         <i className='fab fa-twitter' />
       </a>
     </li>
     <li>
-      <a href='#'>
+      <a href='https://twitter.com/frontendmentor'>
         <i className='fab fa-instagram' />
       </a>
     </li>
